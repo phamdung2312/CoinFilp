79385e7e6ef120c0818300c9eadefdff82ae84d1
phamdung2312 <dungp7674@gmail.com>
2025-09-22 22:46:32 +0700

    updates

--- src/app/component/WelcomeMessage.tsx
+++ src/app/component/WelcomeMessage.tsx
@@ -76,7 +76,7 @@ export default function WelcomeMessage() {
                 fontFamily: "'Arial Black', sans-serif",
               }}
             >
-              CHÀO MỪNG BẠN NICK PHAM DŨNG TEST
+              CHÀO MỪNG BẠN NICK PHAM DŨNG TEST 1
             </h2>
 
             {/* Dòng chữ thứ hai */}
